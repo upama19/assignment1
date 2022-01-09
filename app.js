1770ecd1b8547e2583e29ee28c0691ba432522c9
upama19 <upamashree2000@gmail.com>
2022-01-09 19:21:26 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+
+    res.writeHead(200,{'content-type':'text/html'})
+    res.write('<h1>Home page</h1>  <br> <p> Nothing just bored...</p> ')
+    res.end()
+
+})
+
+server.listen(5000)
+
