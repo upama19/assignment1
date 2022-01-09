4ea1b511de2e44bb076a9b9d8292701f75a236d4
upama19 <upamashree2000@gmail.com>
2022-01-09 19:22:34 +0545

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http')
 const server = http.createServer((req,res)=>{
 
     res.writeHead(200,{'content-type':'text/html'})
-    res.write('<h1>Home page</h1>  <br> <p> Nothing just bored...</p> ')
+    res.write('<h1>Home page</h1>  <br> <p> Nothing just bored. Just learning ..</p> ')
     res.end()
 
 })
